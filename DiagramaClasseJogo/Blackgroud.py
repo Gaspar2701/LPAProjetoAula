0f6d63acc8dee4ffc077b7d421aac779c26799a6
Gaspar2701 <136199829+Gaspar2701@users.noreply.github.com>
2024-03-10 15:14:31 -0300

    Signed-off-by: Gaspar2701 <136199829+Gaspar2701@users.noreply.github.com>

---
+++ DiagramaClasseJogo/Blackgroud.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Blackgroud import Blackgroud
+from Entity import Entity
+
+class Blackgroud(Blackgroud, Entity):
+    def __init__(self):
+        pass
+
+    def move(self, ):
+        pass
+
---
+++ DiagramaClasseJogo/Gamer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Gamer:
+    def __init__(self):
+        self.window = None
+
+    def run(self, ):
+        pass
+
