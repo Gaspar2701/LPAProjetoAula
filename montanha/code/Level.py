d0f895af5c6dac937fc091b9a98802fd7bec6b9c
Gaspar2701 <136199829+Gaspar2701@users.noreply.github.com>
2024-03-17 16:20:46 -0300

    Aula sobre criação de criação de tiros e colisão de telas

--- montanha/code/Level.py
+++ montanha/code/Level.py
@@ -7,8 +7,11 @@ from pygame import Surface, Rect
 from pygame.font import Font
 
 from code.Const import COLOR_WHITE, MENU_OPTION, EVENT_ENEMY
+from code.Enemy import Enemy
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
+from code.EntityMediator import EntityMediator
+from code.Player import Player
 
 
 class Level:
@@ -19,20 +22,36 @@ class Level:
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
         self.entity_list.append(EntityFactory.get_entity('Player1'))
-        if menu_option in [MENU_OPTION[1],MENU_OPTION[2]]:
+        if menu_option in [MENU_OPTION[1], MENU_OPTION[2]]:
             self.entity_list.append(EntityFactory.get_entity('Player2'))
         pygame.time.set_timer(EVENT_ENEMY, 2000)
 
     def run(self):
         pygame.mixer_music.load(f'./asset/{self.name}.mp3')
+        pygame.mixer_music.set_volume(0.3)
         pygame.mixer_music.play(-1)
         clock = pygame.time.Clock()
         while True:
             clock.tick(60)
+            # for para desenhar todas as entidades
             for ent in self.entity_list:
-                self.window.blit(source=ent.surf, dest=ent.rect)# Aqui eu desenho minhas entidades
-                self.level_text(14, f'fps: {clock.get_fps() :.0f}', COLOR_WHITE, (10, 10))
+                self.window.blit(source=ent.surf, dest=ent.rect)  # Aqui eu desenho minhas entidades
                 ent.move()
+                if isinstance(ent, (Player, Enemy)):
+                    shoot = ent.shoot()
+                    if shoot is not None:
+                        self.entity_list.append(shoot)
+            # texto a ser printado na tela
+            self.level_text(14, f'fps: {clock.get_fps() :.0f}', COLOR_WHITE, (10, 10))
+            self.level_text(14, f'entidades: {len(self.entity_list) :.0f}', COLOR_WHITE, (10, 25))
+            # Atualizar tela
+            pygame.display.flip()
+
+            # Verificar relacionamento de entidades
+            EntityMediator.verify_collision(entity_listy=self.entity_list)
+            EntityMediator.verify_health(entity_list=self.entity_list)
+
+            # Conferir eventos
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -40,7 +59,7 @@ class Level:
                 if event.type == EVENT_ENEMY:
                     choice = random.choice(('Enemy1', 'Enemy2'))
                     self.entity_list.append(EntityFactory.get_entity(choice))
-            pygame.display.flip()
+
         pass
 
     def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
--- montanha/code/Player.py
+++ montanha/code/Player.py
@@ -1,30 +1,44 @@
 #!/usr/bin/python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import pygame
 
 from code.Const import ENTITY_SPEED, WIN_HEIGHT, WIN_WIDTH, PLAYER_KEY_UP, PLAYER_KEY_DOWN, PLAYER_KEY_LEFT, \
-    PLAYER_KEY_RIGHT
+    PLAYER_KEY_RIGHT, PLAYER_KEY_SHOOT, ENTITY_SHOT_DELAY
 from code.Entity import Entity
+from code.PlayerShot import PlayerShot
 
 
 class Player(Entity):
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
+        self.shot_delay = ENTITY_SHOT_DELAY[self.name]
 
     def move(self):
         pressed_key = pygame.key.get_pressed()
-        if pressed_key[PLAYER_KEY_UP[self.name]] and self.rect.top > 0: # Se a tecla seta para cima for pess
+        if pressed_key[PLAYER_KEY_UP[self.name]] and self.rect.top > 0:  # Se a tecla seta para cima for pess
             self.rect.centery -= ENTITY_SPEED[self.name]
 
-        if pressed_key[PLAYER_KEY_DOWN[self.name]] and self.rect.bottom < WIN_HEIGHT: # Se a tecla seta para baixo for pess
+        if pressed_key[
+            PLAYER_KEY_DOWN[self.name]] and self.rect.bottom < WIN_HEIGHT:  # Se a tecla seta para baixo for pess
             self.rect.centery += ENTITY_SPEED[self.name]
 
-        if pressed_key[PLAYER_KEY_LEFT[self.name]] and self.rect.left > 0: # Se a tecla seta para direita for pess
+        if pressed_key[PLAYER_KEY_LEFT[self.name]] and self.rect.left > 0:  # Se a tecla seta para direita for pess
             self.rect.centerx -= ENTITY_SPEED[self.name]
 
-        if pressed_key[PLAYER_KEY_RIGHT[self.name]] and self.rect.right < WIN_WIDTH: # Se a tecla seta para esqueda for pess
+        if pressed_key[
+            PLAYER_KEY_RIGHT[self.name]] and self.rect.right < WIN_WIDTH:  # Se a tecla seta para esqueda for pess
             self.rect.centerx += ENTITY_SPEED[self.name]
 
+    def shoot(self):
+        self.shot_delay -= 1
+        if self.shot_delay == 0:
+            self.shot_delay = ENTITY_SHOT_DELAY[self.name]
+            pressed_key = pygame.key.get_pressed()
+            if pressed_key[PLAYER_KEY_SHOOT[self.name]]:
+                return PlayerShot(f'{self.name}Shot', position=(self.rect.centerx, self.rect.centery))
+            else:
+                return None
 
-        pass
+        else:
+            return None
 
