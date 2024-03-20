12ef618e87a85e9276ea27cd9ff0caf3efe33efb
Gaspar2701 <136199829+Gaspar2701@users.noreply.github.com>
2024-03-20 00:12:53 -0300

    Aula sobre criação de pontuação  salva e apresentar na tela e finalização do gamer

--- montanha/code/Menu.py
+++ montanha/code/Menu.py
@@ -6,7 +6,7 @@ from tkinter.font import Font
 import pygame.image
 from pygame import Surface, Rect
 
-from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_YELLOW
+from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_YELLOW, MENU_OPTION_SPACE
 
 
 class Menu:
@@ -27,9 +27,9 @@ class Menu:
 
             for i in range(len(MENU_OPTION)):
                 if i == menu_option:
-                    self.menu_text(20, MENU_OPTION[i], COLOR_YELLOW, ((WIN_WIDTH / 2), 200 + 30 * i))
+                    self.menu_text(20, MENU_OPTION[i], COLOR_YELLOW, (WIN_WIDTH / 2, MENU_OPTION_SPACE[i]))
                 else:
-                    self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 30 * i))
+                    self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, (WIN_WIDTH / 2, MENU_OPTION_SPACE[i]))
 
             # Verificar eventos
 
@@ -54,6 +54,6 @@ class Menu:
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
-        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_surf: Surface = text_font.render (text, True, text_color).convert_alpha()
         text_rect: Rect = text_surf.get_rect(center=text_center_pos)
         self.window.blit(source=text_surf, dest=text_rect)
